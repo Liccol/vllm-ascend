873acda3cffc80cb4181975b6038d1e8be316cba
CaranLic <740821011@qq.com>
2025-09-08 17:58:49 +0800

    add test
    Signed-off-by: CaranLic <740821011@qq.com>

--- tests/ut/ops/test_min_p_logits_processor.py
+++ tests/ut/ops/test_min_p_logits_processor.py
@@ -10,7 +10,7 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
 
     def test_init_func_without_decode_max_num_seqs(self,
                                                    mocker: MockerFixture):
-        mock_min_p_logits_processor = mocker.MockerFixture(
+        mock_min_p_logits_processor = mocker.MagicMock(
             spec=MinPLogitsProcessor)
 
         min_p_logits_processor_init_func(mock_min_p_logits_processor,
@@ -22,13 +22,13 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
 
     def test_init_func_with_decode_max_num_seqs_and_npu(
             self, mocker: MockerFixture):
-        mock_min_p_logits_processor = mocker.MockerFixture(
+        mock_min_p_logits_processor = mocker.MagicMock(
             spec=MinPLogitsProcessor)
 
         mock_vllm_config = mocker.MagicMock(spec=VllmConfig)
         mock_scheduler_config = mocker.MagicMock(spec=SchedulerConfig)
-        mock_scheduler_config.max_num_seqs = 128
         mock_scheduler_config.decode_max_num_seqs = 256
+        mock_scheduler_config.max_num_seqs = 128
         mock_vllm_config.scheduler_config = mock_scheduler_config
         mocker.patch(
             "vllm_ascend.ops.min_p_logits_processor.get_current_vllm_config",
@@ -42,13 +42,13 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
 
     def test_init_func_with_decode_max_num_seqs_and_npu(
             self, mocker: MockerFixture):
-        mock_min_p_logits_processor = mocker.MockerFixture(
+        mock_min_p_logits_processor = mocker.MagicMock(
             spec=MinPLogitsProcessor)
 
         mock_vllm_config = mocker.MagicMock(spec=VllmConfig)
         mock_scheduler_config = mocker.MagicMock(spec=SchedulerConfig)
-        mock_scheduler_config.decode_max_num_seqs = 256
         mock_scheduler_config.max_num_seqs = 128
+        mock_scheduler_config.decode_max_num_seqs = 256
         mock_vllm_config.scheduler_config = mock_scheduler_config
         mocker.patch(
             "vllm_ascend.ops.min_p_logits_processor.get_current_vllm_config",
@@ -57,5 +57,4 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
         min_p_logits_processor_init_func(mock_min_p_logits_processor,
                                          mock_vllm_config, "cpu:0", True)
 
-        assert mock_min_p_logits_processor.min_p_cpu.shape[0] == 256
         assert mock_min_p_logits_processor.use_double_tensor == False
