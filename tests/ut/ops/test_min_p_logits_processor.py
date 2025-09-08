75bd53f17968e3d2a22c2806c77f0c43d25dd4ee
CaranLic <740821011@qq.com>
2025-09-08 20:41:20 +0800

    fix test error for unexpected online ut
    Signed-off-by: CaranLic <740821011@qq.com>

--- tests/ut/ops/test_min_p_logits_processor.py
+++ tests/ut/ops/test_min_p_logits_processor.py
@@ -1,9 +1,10 @@
 from pytest_mock import MockerFixture
-from vllm.config import VllmConfig, SchedulerConfig
+from vllm.config import SchedulerConfig, VllmConfig
 from vllm.v1.sample.logits_processor import MinPLogitsProcessor
 
 from tests.ut.base import PytestBase
-from vllm_ascend.ops.min_p_logits_processor import min_p_logits_processor_init_func
+from vllm_ascend.ops.min_p_logits_processor import \
+    min_p_logits_processor_init_func
 
 
 class TestMinPLogitsProcessorInitFunc(PytestBase):
@@ -12,13 +13,23 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
                                                    mocker: MockerFixture):
         mock_min_p_logits_processor = mocker.MagicMock(
             spec=MinPLogitsProcessor)
+        mock_min_p_logits_processor.min_p_cpu is None
+        mock_vllm_config = mocker.MagicMock(spec=VllmConfig)
+        mock_scheduler_config = mocker.MagicMock(spec=SchedulerConfig)
+        mock_scheduler_config.decode_max_num_seqs = 0
+        mock_scheduler_config.max_num_seqs = 128
+        mock_vllm_config.scheduler_config = mock_scheduler_config
+        mocker.patch(
+            "vllm_ascend.ops.min_p_logits_processor.get_current_vllm_config",
+            return_value=mock_vllm_config)
+        mocker.patch(
+            "vllm_ascend.ops.min_p_logits_processor.original_min_p_logits_processor_init_func",
+            return_value=None)
 
         min_p_logits_processor_init_func(mock_min_p_logits_processor,
-                                         VllmConfig(), "cpu:0", True)
+                                         mock_vllm_config, "cpu", True)
 
-        assert mock_min_p_logits_processor.min_p_cpu is not None
-        assert mock_min_p_logits_processor.min_p_device is not None
-        assert mock_min_p_logits_processor.min_p_cpu.shape[0] == 128
+        assert mock_min_p_logits_processor.min_p_cpu is None
 
     def test_init_func_with_decode_max_num_seqs_and_npu(
             self, mocker: MockerFixture):
@@ -33,14 +44,17 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
         mocker.patch(
             "vllm_ascend.ops.min_p_logits_processor.get_current_vllm_config",
             return_value=mock_vllm_config)
+        mocker.patch(
+            "vllm_ascend.ops.min_p_logits_processor.original_min_p_logits_processor_init_func",
+            return_value=None)
 
         min_p_logits_processor_init_func(mock_min_p_logits_processor,
-                                         mock_vllm_config, "npu:0", True)
+                                         mock_vllm_config, "npu", True)
 
         assert mock_min_p_logits_processor.min_p_cpu.shape[0] == 256
-        assert mock_min_p_logits_processor.use_double_tensor == True
+        assert mock_min_p_logits_processor.use_double_tensor is True
 
-    def test_init_func_with_decode_max_num_seqs_and_npu(
+    def test_init_func_with_decode_max_num_seqs_and_cpu(
             self, mocker: MockerFixture):
         mock_min_p_logits_processor = mocker.MagicMock(
             spec=MinPLogitsProcessor)
@@ -55,6 +69,6 @@ class TestMinPLogitsProcessorInitFunc(PytestBase):
             return_value=mock_vllm_config)
 
         min_p_logits_processor_init_func(mock_min_p_logits_processor,
-                                         mock_vllm_config, "cpu:0", True)
+                                         mock_vllm_config, "cpu", True)
 
-        assert mock_min_p_logits_processor.use_double_tensor == False
+        assert mock_min_p_logits_processor.use_double_tensor is False
